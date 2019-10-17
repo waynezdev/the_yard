8ad42772e42ca72e7d011fed7986163a31318ee6
garretblankenship <garret.blankenship@coderacademy.edu.au>
2019-10-17 11:28:18 +1100

    added in faker

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,6 @@
 class Ingredient < ApplicationRecord
+    has_many :ingredients_milkshakes
+    has_many :milkshakes, through: :ingredients_milkshakes
+    has_one :image, as: :imageable
+    enum type: { base: 0, flavour: 1, topping: 2 }
 end
--- app/models/milkshake.rb
+++ app/models/milkshake.rb
@@ -1,2 +1,5 @@
 class Milkshake < ApplicationRecord
+    has_many :ingredients_milkshakes
+    has_many :ingredients, through: :ingredients_milkshakes
+    has_one :image, as: :imageable
 end
