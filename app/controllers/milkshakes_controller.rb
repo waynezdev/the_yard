67a9e8ba7bb736ed80b7f6e22dce2455d7244747
garretblankenship <garret.blankenship@coderacademy.edu.au>
2019-10-17 15:30:51 +1100

    added styling and links

--- app/controllers/milkshakes_controller.rb
+++ app/controllers/milkshakes_controller.rb
@@ -1,5 +1,13 @@
 class MilkshakesController < ApplicationController
     def index
-        @milkshakes = Milkshake.all
+        if params[:search] && !params[:search].empty?
+            @milkshakes = Milkshake.where(name: params[:search])
+        else
+            @milkshakes = Milkshake.all
+        end
+    end
+
+    def show
+        @milkshake = Milkshake.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to: "pages#home", as: "root"
   get "/milkshakes", to: "milkshakes#index", as: "milkshakes"
+  get "/milkshakes/:id", to: "milkshakes#show", as: "milkshake"
 end
